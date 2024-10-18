arodland/fldigi

commit 9bd273df32722882f476165180729f9d716b6626

color prefs
  * move color definitions from configuration to a separate
    prefs file to allow independent color renderings.
  * specify all colors as a triad: red, green, blue 0 to 255

src/fsq/fsq_monitor.cxx
@@ -29,6 +29,7 @@
 #include "trx.h"
 #include "main.h"
 #include "configuration.h"
+#include "ui_colors.h"
 #include "confdialog.h"
 #include "status.h"
 #include "waterfall.h"
@@ -73,28 +74,18 @@ Fl_Double_Window* create_fsqMonitor(void)
 		fsq_monitor = new FTextRX(
 				monitor_panel->x(), monitor_panel->y(),
 				monitor_panel->w(), 7*monitor_panel->h()/8);
-			fsq_monitor->color(
-			fl_rgb_color(
-				0.98*progdefaults.RxColor.R,
-				0.98*progdefaults.RxColor.G,
-				0.98*progdefaults.RxColor.B),
-				progdefaults.RxTxSelectcolor);
+			fsq_monitor->color( 0.98 * RGBCOLOR( RxColor ), RGBCOLOR( RxTxSelectcolor ) );
 			fsq_monitor->setFont(progdefaults.RxFontnbr);
 			fsq_monitor->setFontSize(progdefaults.RxFontsize);
-			fsq_monitor->setFontColor(progdefaults.RxFontcolor, FTextBase::RECV);
+			fsq_monitor->setFontColor( RGBCOLOR( RxFontcolor ), FTextBase::RECV);
 
 		fsq_que = new FTextRX(
 				fsq_monitor->x(), fsq_monitor->y() + fsq_monitor->h(),
 				fsq_monitor->w(), monitor_panel->h() - fsq_monitor->h());
-			fsq_que->color(
-			fl_rgb_color(
-				0.98*progdefaults.RxColor.R,
-				0.98*progdefaults.RxColor.G,
-				0.98*progdefaults.RxColor.B),
-				progdefaults.RxTxSelectcolor);
+			fsq_que->color( 0.98 * RGBCOLOR( RxColor ), RGBCOLOR( RxTxSelectcolor ) );
 			fsq_que->setFont(progdefaults.RxFontnbr);
 			fsq_que->setFontSize(progdefaults.RxFontsize);
-			fsq_que->setFontColor(progdefaults.RxFontcolor, FTextBase::RECV);
+			fsq_que->setFontColor(RGBCOLOR( RxFontcolor ), FTextBase::RECV);
 
 		Fl_Box *minbox = new Fl_Box(
 			monitor_panel->x(), monitor_panel->y() + 66,

src/misc/configuration.cxx
@@ -29,6 +29,7 @@
 #include <config.h>
 
 #include "configuration.h"
+#include "ui_colors.h"
 #include "confdialog.h"
 #include "xmlreader.h"
 #include "soundconf.h"
@@ -444,15 +445,15 @@ void configuration::saveDefaults()
 {
 	ENSURE_THREAD(FLMAIN_TID);
 
-	memcpy(&cfgpal0, &palette[0], sizeof(cfgpal0));
-	memcpy(&cfgpal1, &palette[1], sizeof(cfgpal1));
-	memcpy(&cfgpal2, &palette[2], sizeof(cfgpal2));
-	memcpy(&cfgpal3, &palette[3], sizeof(cfgpal3));
-	memcpy(&cfgpal4, &palette[4], sizeof(cfgpal4));
-	memcpy(&cfgpal5, &palette[5], sizeof(cfgpal5));
-	memcpy(&cfgpal6, &palette[6], sizeof(cfgpal6));
-	memcpy(&cfgpal7, &palette[7], sizeof(cfgpal7));
-	memcpy(&cfgpal8, &palette[8], sizeof(cfgpal8));
+//	ui_colors.cfgpal0 = fl_rgb_color( palette[0].R, palette[0].G, palette[0].B);
+//	ui_colors.cfgpal1 = fl_rgb_color( palette[1].R, palette[1].G, palette[1].B);
+//	ui_colors.cfgpal2 = fl_rgb_color( palette[2].R, palette[2].G, palette[2].B);
+//	ui_colors.cfgpal3 = fl_rgb_color( palette[3].R, palette[3].G, palette[3].B);
+//	ui_colors.cfgpal4 = fl_rgb_color( palette[4].R, palette[4].G, palette[4].B);
+//	ui_colors.cfgpal5 = fl_rgb_color( palette[5].R, palette[5].G, palette[5].B);
+//	ui_colors.cfgpal6 = fl_rgb_color( palette[6].R, palette[6].G, palette[6].B);
+//	ui_colors.cfgpal7 = fl_rgb_color( palette[7].R, palette[7].G, palette[7].B);
+//	ui_colors.cfgpal8 = fl_rgb_color( palette[8].R, palette[8].G, palette[8].B);
 
 	RxFontName = Fl::get_font_name(RxFontnbr);
 	TxFontName = Fl::get_font_name(TxFontnbr);
@@ -598,12 +599,9 @@ int configuration::setDefaults()
 	cntServerOffset->value(ServerOffset);
 	cntACQsn->value(ACQsn);
 
-	btnCursorBWcolor->color(
-		fl_rgb_color(cursorLineRGBI.R, cursorLineRGBI.G, cursorLineRGBI.B) );
-	btnCursorCenterLineColor->color(
-		fl_rgb_color(cursorCenterRGBI.R, cursorCenterRGBI.G, cursorCenterRGBI.B) );
-	btnBwTracksColor->color(
-		fl_rgb_color(bwTrackRGBI.R, bwTrackRGBI.G, bwTrackRGBI.B) );
+	btnCursorBWcolor->color( RGBCOLOR( cursorLine ) );
+	btnCursorCenterLineColor->color( RGBCOLOR( cursorCenter ) );
+	btnBwTracksColor->color( RGBCOLOR( bwTrack ) );
 
 	sldrCWxmtWPM->value(CWspeed);
 	cntCWdefWPM->value(defCWspeed);
@@ -681,15 +679,15 @@ int configuration::setDefaults()
 	wf->setPrefilter(wfPreFilter);
 	btnWFaveraging->value(WFaveraging);
 
-	memcpy(&palette[0], &cfgpal0, sizeof(palette[0]));
-	memcpy(&palette[1], &cfgpal1, sizeof(palette[1]));
-	memcpy(&palette[2], &cfgpal2, sizeof(palette[2]));
-	memcpy(&palette[3], &cfgpal3, sizeof(palette[3]));
-	memcpy(&palette[4], &cfgpal4, sizeof(palette[4]));
-	memcpy(&palette[5], &cfgpal5, sizeof(palette[5]));
-	memcpy(&palette[6], &cfgpal6, sizeof(palette[6]));
-	memcpy(&palette[7], &cfgpal7, sizeof(palette[7]));
-	memcpy(&palette[8], &cfgpal8, sizeof(palette[8]));
+	Fl::get_color( RGBCOLOR( cfgpal0 ), palette[0].R, palette[0].G, palette[0].B);
+	Fl::get_color( RGBCOLOR( cfgpal1 ), palette[1].R, palette[1].G, palette[1].B);
+	Fl::get_color( RGBCOLOR( cfgpal2 ), palette[2].R, palette[2].G, palette[2].B);
+	Fl::get_color( RGBCOLOR( cfgpal3 ), palette[3].R, palette[3].G, palette[3].B);
+	Fl::get_color( RGBCOLOR( cfgpal4 ), palette[4].R, palette[4].G, palette[4].B);
+	Fl::get_color( RGBCOLOR( cfgpal5 ), palette[5].R, palette[5].G, palette[5].B);
+	Fl::get_color( RGBCOLOR( cfgpal6 ), palette[6].R, palette[6].G, palette[6].B);
+	Fl::get_color( RGBCOLOR( cfgpal7 ), palette[7].R, palette[7].G, palette[7].B);
+	Fl::get_color( RGBCOLOR( cfgpal8 ), palette[8].R, palette[8].G, palette[8].B);
 
 	wf->setcolors();
 	setColorButtons();
@@ -719,9 +717,9 @@ int configuration::setDefaults()
 	uchar bg_r, bg_g, bg_b;
 	uchar bg2_r, bg2_g, bg2_b;
 
-	Fl::get_color(progdefaults.foreground, fg_r, fg_g, fg_b);
-	Fl::get_color(progdefaults.background, bg_r, bg_g, bg_b);
-	Fl::get_color(progdefaults.background2, bg2_r, bg2_g, bg2_b);
+	Fl::get_color(RGBCOLOR( foreground ), fg_r, fg_g, fg_b);
+	Fl::get_color(RGBCOLOR( background ), bg_r, bg_g, bg_b);
+	Fl::get_color(RGBCOLOR( background2 ), bg2_r, bg2_g, bg2_b);
 
 	Fl::background2(bg2_r, bg2_g, bg2_b);
 	Fl::background(bg_r, bg_g, bg_b);

src/status/ui_colors.cxx
@@ -1,0 +1,187 @@
+//---------------------------------------------------------------------
+// ui_colors.cxx
+//
+// Copyright (C) 2024
+//		Dave Freese; W1HKJ
+//
+// This file is part of fldigi.
+//
+// Fldigi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License; or
+// (at your option) any later version.
+//
+// Fldigi is distributed in the hope that it will be useful;
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with fldigi.  If not; see <http://www.gnu.org/licenses/>.
+//----------------------------------------------------------------------
+#include <config.h>
+#include <iostream>
+#include <fstream>
+
+#include "ui_colors.h"
+
+#include "main.h"
+#include "waterfall.h"
+
+UI_COLORS ui_colors = { // red, green, blue, var
+  {  192, 192, 192,	"background" },
+  {  255, 255, 255,	"background2" },
+  {  0, 0, 0,		"foreground" },
+  {  255, 255, 255,	"rsid" },
+  {  0, 0, 0,		"digi_background" },
+  {  255, 255, 255,	"digi_axis_color" },
+  {  0, 255, 0,		"digi_color_1" },
+  {  255, 255, 0,	"digi_color_2" },
+  {  0, 255, 255,	"digi_color_3" },
+  {  255, 0, 255,	"digi_color_4" },
+  {  255, 255, 0,	"cursorLine" },
+  {  255, 255, 255,	"cursorCenter" },
+  {  255, 0, 0,		"bwTrack" },
+  {  255, 255, 255,	"notch" },
+  {  255, 120, 0,	"rttymark" },
+  {  0, 255, 0,		"monitor" },
+  {  185, 211, 238,	"bwsrSliderColor" },
+  {  54, 100, 139,	"bwsrSldrSelColor" },
+  {  255, 0, 0,		"bwsrHiLight1" },
+  {  0, 255, 0,		"bwsrHiLight2" },
+  {  255, 255, 255,	"bwsrBackgnd1" },
+  {  234, 234, 234,	"bwsrBackgnd2" },
+  {  0, 0, 255,		"bwsrSelect" },
+  {  255, 110, 180,	"dup_color" },
+  {  255, 255, 150,	"possible_dup_color" },
+  {  0, 0, 130,		"DX_Color" },
+  {  255, 255, 225,	"DXC_even_color" },
+  {  191, 218, 255,	"DXC_odd_color" },
+  {  255, 255, 0,	"DXC_textcolor" },
+  {  255, 255, 0,	"DXfontcolor" },
+  {  127, 0, 0,		"DXalt_color" },
+  {  0, 0, 0,		"cfgpal0" },
+  {  0, 0, 136,		"cfgpal1" },
+  {  0, 19, 198,	"cfgpal2" },
+  {  0, 32, 239,	"cfgpal3" },
+  {  172, 167, 105,	"cfgpal4" },
+  {  195, 198, 49,	"cfgpal5" },
+  {  225, 228, 107,	"cfgpal6" },
+  {  255, 255, 0,	"cfgpal7" },
+  {  251, 51, 0,	"cfgpal8" },
+  {  0, 0, 0,		"RxFontcolor" },
+  {  255, 255, 255,	"MacroBtnFontcolor" },
+  {  255, 0, 255,	"RxTxSelectcolor" },
+  {  255, 0, 0,		"TxFontWarn" },
+  {  0, 0, 0,		"TxFontcolor" },
+  {  255, 242, 190,	"RxColor" },
+  {  200, 235, 255,	"TxColor" },
+  {  255, 0, 0,		"XMITcolor" },
+  {  0, 145, 0,		"CTRLcolor" },
+  {  0, 0, 255,		"SKIPcolor" },
+  {  127, 0, 127,	"ALTRcolor" },
+  {  0, 0, 0,		"LowSignal" },
+  {  0, 255, 0,		"NormSignal	" },
+  {  255, 255, 0,	"HighSignal	" },
+  {  255, 0, 0,		"OverSignal	" },
+  {  0, 0, 0,		"LOGGINGtextcolor" },
+  {  255, 255, 255,	"LOGGINGcolor" },
+  {  0, 0, 0,		"LOGBOOKtextcolor" },
+  {  255, 255, 255,	"LOGBOOKcolor" },
+  {  255, 253, 222,	"FDbackground" },
+  {  0, 0, 0,		"FDforeground" },
+  {  255, 253, 222,	"Smeter_bg_color" },
+  {  0, 0, 0,		"Smeter_scale_color" },
+  {  0, 200, 0,		"Smeter_meter_color" },
+  {  255, 253, 222,	"PWRmeter_bg_color" },
+  {  0, 0, 0,		"PWRmeter_scale_color "},
+  {  200, 0, 0,		"PWRmeter_meter_color" },
+  {  255, 255, 255,	"TabsColor" },
+  {  255, 255, 0,	"Sql1Color" },
+  {  0, 255, 0,		"Sql2Color" },
+  {  0, 255, 0,		"AfcColor" },
+  {  0, 255, 0,		"LkColor" },
+  {  0, 255, 0,		"RevColor" },
+  {  255, 0, 0,		"XmtColor" },
+  {  0, 255, 0,		"SpotColor" },
+  {  0, 255, 0,		"RxIDColor" },
+  {  127, 0, 0,		"RxIDwideColor" },
+  {  0, 255, 0,		"TxIDColor" },
+  {  255, 0, 0,		"TuneColor" },
+  {  0, 255, 0,		"default_btn_color" },
+  {  255, 255, 0,	"default_check_btn_color" },
+  {  0, 255, 0,		"default_round_btn_color" },
+  {  255, 255, 255,	"FMT_background" },
+  {  0, 0, 127,		"FMT_unk_color" },
+  {  0, 255, 0,		"FMT_ref_color" },
+  {  0, 0, 0,		"FMT_legend_color" },
+  {  0, 0, 0,		"FMT_axis_color" },
+  {  80, 144, 144,	"btnGroup1" },
+  {  144, 80, 80,	"btnGroup2" },
+  {  80, 80, 144,	"btnGroup3" },
+  {  0, 145, 0,		"fsq_undirected_color" },
+  {  0, 0, 255,		"fsq_directed_color" },
+  {  255, 0, 0,		"fsq_xmt_color" },
+  {  255, 255, 255,	"btnFkeyTextColor" },
+  {  0, 0, 0, "" }
+};
+
+void UI_COLORS::load(std::string fname)
+{
+	TRIAD triad;
+	triad.var.reserve(100);
+
+	std::string filename = HomeDir;
+	filename.append("/").append(fname).append(".colors");
+	std::ifstream clrfile;
+	clrfile.open( filename.c_str());
+	std::string line;
+	char chline[100];
+	char chvar[50];
+	TRIAD *ptr;
+	clrfile.getline( chline, 100 );
+	if ( (line = chline).empty()) {
+		clrfile.close();
+		return;
+	}
+	while (!clrfile.eof()) {
+		clrfile.getline( chline, 100 );
+		line = chline;
+		if (!line.empty()) {
+			sscanf(line.c_str(), "%s (%hhu)(%hhu)(%hhu)", chvar, &triad.r, &triad.g, &triad.b);
+			ptr = &background;
+			while (!ptr->var.empty()) {
+				if (ptr->var == chvar) {
+					ptr->r = triad.r; ptr->g = triad.g, ptr->b = triad.b;
+//			std::cout << "FOUND: " << chvar << ", " << (uint)triad.r << ", " << (uint)triad.g << ", " << (uint)triad.b << std::endl;
+					break;
+				}
+				ptr++;
+			}
+		}
+	}
+	clrfile.close();
+}
+
+char * UI_COLORS::rgbstr(TRIAD clr)
+{
+	static char sz[50];
+	snprintf(sz, sizeof(sz), "(%u)(%u)(%u)", clr.r, clr.g, clr.b);
+	return sz;
+}
+
+void UI_COLORS::save(std::string fname)
+{
+
+	std::string filename = HomeDir;
+	filename.append("/").append(fname).append(".colors");
+	FILE *clrfile = fopen( filename.c_str(), "w");
+
+	TRIAD *ptr = &background;
+	fprintf(clrfile, "fldigi color definition file: <control> (red)(green)(blue)\n");
+	while (!ptr->var.empty()){
+		fprintf(clrfile, "%s %s\n", ptr->var.c_str(), rgbstr(*ptr));
+		ptr++;
+	}
+	fclose(clrfile);
+}

src/waterfall/waterfall.cxx
@@ -61,6 +61,7 @@
 
 #include "fldigi-config.h"
 #include "configuration.h"
+#include "ui_colors.h"
 #include "status.h"
 #include "squelch_status.h"
 #include "Viewer.h"
@@ -160,10 +161,8 @@ WFdisp::WFdisp (int x0, int y0, int w0, int h0, char *lbl) :
 	bandwidth = 32;
 	RGBmarker = RGBred;
 	RGBcursor = RGByellow;
-	RGBInotch.I = progdefaults.notchRGBI.I;
-	RGBInotch.R = progdefaults.notchRGBI.R;
-	RGBInotch.G = progdefaults.notchRGBI.G;
-	RGBInotch.B = progdefaults.notchRGBI.B;
+	RGBInotch.I = 255;
+	Fl::get_color( RGBCOLOR( notch ), RGBInotch.R, RGBInotch.G, RGBInotch.B );
 	mode = WATERFALL;
 	centercarrier = false;
 	overload = false;
@@ -305,9 +304,8 @@ void WFdisp::make_fmt_marker ()
 
 	memset(markerimage + scale_width, 0, RGBwidth * (WFMARKER - 2));
 
-	Fl::get_color(progdefaults.FMT_unk_color, unk_trk_color.R, unk_trk_color.G, unk_trk_color.B);
-
-	Fl::get_color(progdefaults.FMT_ref_color, ref_trk_color.R, ref_trk_color.G, ref_trk_color.B);
+	Fl::get_color(RGBCOLOR( FMT_unk_color ), unk_trk_color.R, unk_trk_color.G, unk_trk_color.B);
+	Fl::get_color(RGBCOLOR( FMT_ref_color ), ref_trk_color.R, ref_trk_color.G, ref_trk_color.B);
 
 	int fmt_bw = progdefaults.FMT_filter;
 
@@ -354,9 +352,7 @@ void WFdisp::makeMarker()
 					progdefaults.rtty_custom_shift));
 		marker_width = (int)(marker_width / 2.0 + 1);
 
-		RGBmarker.R = progdefaults.bwTrackRGBI.R;
-		RGBmarker.G = progdefaults.bwTrackRGBI.G;
-		RGBmarker.B = progdefaults.bwTrackRGBI.B;
+		Fl::get_color( RGBCOLOR( bwTrack ), RGBmarker.R, RGBmarker.G, RGBmarker.B );
 
 		makeMarker_(marker_width, &RGBmarker, carrierfreq, clrMin, clrM, clrMax);
 
@@ -383,9 +379,7 @@ void WFdisp::makeMarker()
 	if (xp < bandwidth / 2.0 || xp > (progdefaults.HighFreqCutoff - bandwidth / 2.0))
 		return;
 	clrM = markerimage + scale_width + (int)(xp + 0.5);
-	RGBcursor.R = progdefaults.cursorLineRGBI.R;
-	RGBcursor.G = progdefaults.cursorLineRGBI.G;
-	RGBcursor.B = progdefaults.cursorLineRGBI.B;
+	Fl::get_color( RGBCOLOR( cursorLine ), RGBcursor.R, RGBcursor.G, RGBcursor.B );
 
 	int bw_lo = marker_width;
 	int bw_hi = marker_width;
@@ -1089,18 +1083,21 @@ case Step: for (int row = 0; row < image_height; row++) { \
 		int trk_high = progdefaults.RxFilt_high;
 		RGBI *pos1 = fft_img + trk_low / step;
 		RGBI *pos2 = fft_img + trk_high / step;
+		RGBI clr;
+		clr.I = 255;
+		Fl::get_color( RGBCOLOR( monitor ), clr.R, clr.G, clr.B );
 		if (likely(pos1 >= fft_img && pos2 < fft_img + disp_width)) {
 				if (progdefaults.mon_wide_tracks) {
 					for (int y = 0; y < image_height; y ++) {
-						*(pos1 + 1) = *pos1 = progdefaults.monitorRGBI;
-						*(pos2 - 1) = *pos2 = progdefaults.monitorRGBI;
+						*(pos1 + 1) = *pos1 = clr; //progdefaults.monitorRGBI;
+						*(pos2 - 1) = *pos2 = clr; //progdefaults.monitorRGBI;
 						pos1 += disp_width;
 						pos2 += disp_width;
 					}
 				} else {
 					for (int y = 0; y < image_height; y ++) {
-						*pos1 = progdefaults.monitorRGBI;
-						*pos2 = progdefaults.monitorRGBI;
+						*pos1 = clr; //progdefaults.monitorRGBI;
+						*pos2 = clr; //progdefaults.monitorRGBI;
 						pos1 += disp_width;
 						pos2 += disp_width;
 					}
@@ -1114,10 +1111,8 @@ case Step: for (int row = 0; row < image_height; row++) { \
 		( (carrierfreq - progdefaults.rsid_min_bw / 2) > 1) && 
 		( (carrierfreq + progdefaults.rsid_min_bw / 2) < (progdefaults.HighFreqCutoff - 1))) {
 		RGBI RGBIrsid;
-		RGBIrsid.I = progdefaults.rsidRGBI.I;
-		RGBIrsid.R = progdefaults.rsidRGBI.R;
-		RGBIrsid.G = progdefaults.rsidRGBI.G;
-		RGBIrsid.B = progdefaults.rsidRGBI.B;
+		RGBIrsid.I = 255;//progdefaults.rsidRGBI.I;
+		Fl::get_color( RGBCOLOR( rsid ), RGBIrsid.R, RGBIrsid.G, RGBIrsid.B );
 		RGBI  *pos1 = fft_img + (carrierfreq - offset - progdefaults.rsid_min_bw / 2) / step;
 		RGBI  *pos2 = fft_img + (carrierfreq - offset + progdefaults.rsid_min_bw / 2) / step;
 		int dash = 0;
@@ -1141,8 +1136,8 @@ case Step: for (int row = 0; row < image_height; row++) { \
 			RGBI  *pos2 = fft_img + (trk - offset + bw) / step;
 			RGBI unk_trk_color, ref_trk_color;
 
-			Fl::get_color(progdefaults.FMT_unk_color, unk_trk_color.R, unk_trk_color.G, unk_trk_color.B);
-			Fl::get_color(progdefaults.FMT_ref_color, ref_trk_color.R, ref_trk_color.G, ref_trk_color.B);
+			Fl::get_color(RGBCOLOR( FMT_unk_color ), unk_trk_color.R, unk_trk_color.G, unk_trk_color.B);
+			Fl::get_color(RGBCOLOR( FMT_ref_color ), ref_trk_color.R, ref_trk_color.G, ref_trk_color.B);
 
 			if (likely(pos1 >= fft_img && pos2 < fft_img + disp_width)) {
 				if (progdefaults.UseWideTracks) {
@@ -1203,15 +1198,21 @@ case Step: for (int row = 0; row < image_height; row++) { \
 
 				if (mode == MODE_RTTY && progdefaults.useMARKfreq) {
 					if (active_modem->get_reverse()) {
-						rgbi1 = progdefaults.rttymarkRGBI;
-						rgbi2 = progdefaults.bwTrackRGBI;
+//						rgbi1 = progdefaults.rttymarkRGBI;
+//						rgbi2 = progdefaults.bwTrackRGBI;
+						Fl::get_color( RGBCOLOR( rttymark ), rgbi1.R, rgbi1.G, rgbi1.B ); rgbi1.I = 255;
+						Fl::get_color( RGBCOLOR( bwTrack ), rgbi1.R, rgbi2.G, rgbi2.B ); rgbi2.I = 255;
 					} else {
-						rgbi1 = progdefaults.bwTrackRGBI;
-						rgbi2 = progdefaults.rttymarkRGBI;
+//						rgbi1 = progdefaults.bwTrackRGBI;
+//						rgbi2 = progdefaults.rttymarkRGBI;
+						Fl::get_color( RGBCOLOR( bwTrack ), rgbi1.R, rgbi1.G, rgbi1.B ); rgbi1.I = 255;
+						Fl::get_color( RGBCOLOR( rttymark ), rgbi2.R, rgbi2.G, rgbi2.B ); rgbi2.I = 255;
 					}
 				} else {
-					rgbi1 = progdefaults.bwTrackRGBI;
-					rgbi2 = progdefaults.bwTrackRGBI;
+//					rgbi1 = progdefaults.bwTrackRGBI;
+//					rgbi2 = progdefaults.bwTrackRGBI;
+					Fl::get_color( RGBCOLOR( bwTrack ), rgbi1.R, rgbi1.G, rgbi1.B ); rgbi1.I = 255;
+					rgbi2 = rgbi1;
 				}
 				if (progdefaults.UseWideTracks) {
 					for (int y = 0; y < image_height; y ++) {
@@ -1234,10 +1235,8 @@ case Step: for (int row = 0; row < image_height; row++) { \
 
 // draw notch
 	if ((notch_frequency > 1) && (notch_frequency < progdefaults.HighFreqCutoff - 1)) {
-		RGBInotch.I = progdefaults.notchRGBI.I;
-		RGBInotch.R = progdefaults.notchRGBI.R;
-		RGBInotch.G = progdefaults.notchRGBI.G;
-		RGBInotch.B = progdefaults.notchRGBI.B;
+		RGBInotch.I = 255;
+		Fl::get_color( RGBCOLOR( notch ), RGBInotch.R, RGBInotch.G, RGBInotch.B );
 		RGBI  *notch = fft_img + (notch_frequency - offset) / step;
 		int dash = 0;
 		for (int y = 0; y < image_height; y++) {
@@ -1274,15 +1273,18 @@ void WFdisp::drawcolorWF() {
 		RGBI  *pos0 = (fft_img + cursorpos);
 		RGBI  *pos1 = (fft_img + cursorpos - bw_lo/step);
 		RGBI  *pos2 = (fft_img + cursorpos + bw_hi/step);
+		RGBI  clr;
+		clr.I = 255;
+		Fl::get_color( RGBCOLOR( cursorCenter ), clr.R, clr.G, clr.B );
 		if (pos1 >= fft_img && pos2 < fft_img + disp_width) {
 			for (int y = 0; y < image_height; y ++) {
 				if (progdefaults.UseCursorLines) {
-					*pos1 = *pos2 = progdefaults.cursorLineRGBI;
+					*pos1 = *pos2 = clr;
 					if (progdefaults.UseWideCursor)
 						*(pos1 + 1) = *(pos2 - 1) = *pos1;
 				}
 				if (progdefaults.UseCursorCenterLine) {
-					*pos0 = progdefaults.cursorCenterRGBI;
+					*pos0 = clr;
 					if (progdefaults.UseWideCenter)
 						*(pos0 - 1) = *(pos0 + 1) = *pos0;
 				}
@@ -1664,7 +1666,7 @@ void xmtrcv_cb(Fl_Widget *w, void *vi)
 	} else {
 		if (progdefaults.show_psm_btn && progStatus.kpsql_enabled) {
 			psm_transmit_ended(PSM_ABORT);
-			xmtrcv_selection_color(progdefaults.XmtColor);
+			xmtrcv_selection_color(RGBCOLOR( XmtColor ));
 		}
 
 		if (btnTune->value()) {
@@ -2014,12 +2016,12 @@ waterfall::waterfall(int x0, int y0, int w0, int h0, char *lbl) :
 			w() - 2 * BEZEL,
 			wf_dim - 2 * BEZEL);
 		wfscope = new Digiscope (x() + w(), y(), wf_dim, wf_dim);
-		wfscope->bk_color(progdefaults.digi_background);
-		wfscope->axis_color(progdefaults.digi_axis_color);
-		wfscope->user_color_1(progdefaults.digi_color_1);
-		wfscope->user_color_2(progdefaults.digi_color_2);
-		wfscope->user_color_3(progdefaults.digi_color_3);
-		wfscope->user_color_4(progdefaults.digi_color_4);
+		wfscope->bk_color(RGBCOLOR( digi_background ));
+		wfscope->axis_color(RGBCOLOR( digi_axis_color ));
+		wfscope->user_color_1(RGBCOLOR( digi_color_1 ));
+		wfscope->user_color_2(RGBCOLOR( digi_color_2 ));
+		wfscope->user_color_3(RGBCOLOR( digi_color_3 ));
+		wfscope->user_color_4(RGBCOLOR( digi_color_4 ));
 		rs1->resizable(wfdisp);
 	rs1->end();
 	wfscope->hide();
@@ -2104,7 +2106,7 @@ waterfall::waterfall(int x0, int y0, int w0, int h0, char *lbl) :
 	xmtlock = new Fl_Light_Button(xpos, buttonrow, (int)(bwXmtLock*ratio), BTN_HEIGHT, "Lk");
 	xmtlock->callback(xmtlock_cb, 0);
 	xmtlock->value(0);
-	xmtlock->selection_color(progdefaults.LkColor);
+	xmtlock->selection_color(RGBCOLOR( LkColor ));
 	xmtlock->tooltip(_("Lock transmit frequency"));
 
 	/// We save this flag which is used by rtty decoding.
@@ -2113,13 +2115,13 @@ waterfall::waterfall(int x0, int y0, int w0, int h0, char *lbl) :
 	btnRev->callback(btnRev_cb, 0);
 	reverse = progdefaults.rtty_reverse;
 	btnRev->value(reverse);
-	btnRev->selection_color(progdefaults.RevColor);
+	btnRev->selection_color(RGBCOLOR( RevColor ));
 	btnRev->tooltip(_("Reverse"));
 
 	xpos = w() - (int)(bwXmtRcv*ratio) - wSpace;
 	xmtrcv = new Fl_Light_Button(xpos, buttonrow, (int)(bwXmtRcv*ratio) - BEZEL, BTN_HEIGHT, "T/R");
 	xmtrcv->callback(xmtrcv_cb, 0);
-	xmtrcv->selection_color(progdefaults.XmtColor);
+	xmtrcv->selection_color(RGBCOLOR( XmtColor ));
 	xmtrcv->value(0);
 	xmtrcv->tooltip(_("Transmit/Receive"));
 	end();

src/Makefile.am
@@ -668,6 +668,7 @@ fldigi_SOURCES += \
 	ssb/ssb.cxx \
 	status/squelch_status.cxx \
 	status/status.cxx \
+	status/ui_colors.cxx \
 	synop-src/synop.cxx \
 	thor/thor.cxx \
 	thor/thorvaricode.cxx \
@@ -904,6 +905,7 @@ EXTRA_fldigi_SOURCES += \
 	include/serial.h \
 	include/smeter.h \
 	include/squelch_status.h \
+	include/ui_colors.h \
 	include/vumeter.h \
 	include/socket.h \
 	include/soundconf.h \

commit b4e763fe467d8067c4b420d53c4859327fed02c9

ui_colors
  * fix bugs introduced by commit 9bd273df32722882f4761651

src/fsq/fsq_monitor.cxx
@@ -74,7 +74,7 @@ Fl_Double_Window* create_fsqMonitor(void)
 		fsq_monitor = new FTextRX(
 				monitor_panel->x(), monitor_panel->y(),
 				monitor_panel->w(), 7*monitor_panel->h()/8);
-			fsq_monitor->color( 0.98 * RGBCOLOR( RxColor ), RGBCOLOR( RxTxSelectcolor ) );
+			fsq_monitor->color( RGBCOLOR( RxColor ), RGBCOLOR( RxTxSelectcolor ) );
 			fsq_monitor->setFont(progdefaults.RxFontnbr);
 			fsq_monitor->setFontSize(progdefaults.RxFontsize);
 			fsq_monitor->setFontColor( RGBCOLOR( RxFontcolor ), FTextBase::RECV);
@@ -82,7 +82,7 @@ Fl_Double_Window* create_fsqMonitor(void)
 		fsq_que = new FTextRX(
 				fsq_monitor->x(), fsq_monitor->y() + fsq_monitor->h(),
 				fsq_monitor->w(), monitor_panel->h() - fsq_monitor->h());
-			fsq_que->color( 0.98 * RGBCOLOR( RxColor ), RGBCOLOR( RxTxSelectcolor ) );
+			fsq_que->color( RGBCOLOR( RxColor ), RGBCOLOR( RxTxSelectcolor ) );
 			fsq_que->setFont(progdefaults.RxFontnbr);
 			fsq_que->setFontSize(progdefaults.RxFontsize);
 			fsq_que->setFontColor(RGBCOLOR( RxFontcolor ), FTextBase::RECV);

src/main.cxx
@@ -1240,6 +1240,9 @@ int main (int argc, char *argv[])
 		KmlServer::GetInstance()->Reset();
 	}
 
+	wf->setcolors();
+	setColorButtons();
+
 // OS X will prevent the main window from being resized if we change its
 // size *after* it has been shown. With some X11 window managers, OTOH,
 // the main window will not be restored at its exact saved position if

src/misc/configuration.cxx
@@ -445,15 +445,15 @@ void configuration::saveDefaults()
 {
 	ENSURE_THREAD(FLMAIN_TID);
 
-//	ui_colors.cfgpal0 = fl_rgb_color( palette[0].R, palette[0].G, palette[0].B);
-//	ui_colors.cfgpal1 = fl_rgb_color( palette[1].R, palette[1].G, palette[1].B);
-//	ui_colors.cfgpal2 = fl_rgb_color( palette[2].R, palette[2].G, palette[2].B);
-//	ui_colors.cfgpal3 = fl_rgb_color( palette[3].R, palette[3].G, palette[3].B);
-//	ui_colors.cfgpal4 = fl_rgb_color( palette[4].R, palette[4].G, palette[4].B);
-//	ui_colors.cfgpal5 = fl_rgb_color( palette[5].R, palette[5].G, palette[5].B);
-//	ui_colors.cfgpal6 = fl_rgb_color( palette[6].R, palette[6].G, palette[6].B);
-//	ui_colors.cfgpal7 = fl_rgb_color( palette[7].R, palette[7].G, palette[7].B);
-//	ui_colors.cfgpal8 = fl_rgb_color( palette[8].R, palette[8].G, palette[8].B);
+	ui_colors.cfgpal0.r = palette[0].R; ui_colors.cfgpal0.g = palette[0].G; ui_colors.cfgpal0.b = palette[0].B;
+	ui_colors.cfgpal1.r = palette[1].R; ui_colors.cfgpal1.g = palette[1].G; ui_colors.cfgpal1.b = palette[1].B;
+	ui_colors.cfgpal2.r = palette[2].R; ui_colors.cfgpal2.g = palette[2].G; ui_colors.cfgpal2.b = palette[2].B;
+	ui_colors.cfgpal3.r = palette[3].R; ui_colors.cfgpal3.g = palette[3].G; ui_colors.cfgpal3.b = palette[3].B;
+	ui_colors.cfgpal4.r = palette[4].R; ui_colors.cfgpal4.g = palette[4].G; ui_colors.cfgpal4.b = palette[4].B;
+	ui_colors.cfgpal5.r = palette[5].R; ui_colors.cfgpal5.g = palette[5].G; ui_colors.cfgpal5.b = palette[5].B;
+	ui_colors.cfgpal6.r = palette[6].R; ui_colors.cfgpal6.g = palette[6].G; ui_colors.cfgpal6.b = palette[6].B;
+	ui_colors.cfgpal7.r = palette[7].R; ui_colors.cfgpal7.g = palette[7].G; ui_colors.cfgpal7.b = palette[7].B;
+	ui_colors.cfgpal8.r = palette[8].R; ui_colors.cfgpal8.g = palette[8].G; ui_colors.cfgpal8.b = palette[8].B;
 
 	RxFontName = Fl::get_font_name(RxFontnbr);
 	TxFontName = Fl::get_font_name(TxFontnbr);
@@ -679,15 +679,25 @@ int configuration::setDefaults()
 	wf->setPrefilter(wfPreFilter);
 	btnWFaveraging->value(WFaveraging);
 
-	Fl::get_color( RGBCOLOR( cfgpal0 ), palette[0].R, palette[0].G, palette[0].B);
-	Fl::get_color( RGBCOLOR( cfgpal1 ), palette[1].R, palette[1].G, palette[1].B);
-	Fl::get_color( RGBCOLOR( cfgpal2 ), palette[2].R, palette[2].G, palette[2].B);
-	Fl::get_color( RGBCOLOR( cfgpal3 ), palette[3].R, palette[3].G, palette[3].B);
-	Fl::get_color( RGBCOLOR( cfgpal4 ), palette[4].R, palette[4].G, palette[4].B);
-	Fl::get_color( RGBCOLOR( cfgpal5 ), palette[5].R, palette[5].G, palette[5].B);
-	Fl::get_color( RGBCOLOR( cfgpal6 ), palette[6].R, palette[6].G, palette[6].B);
-	Fl::get_color( RGBCOLOR( cfgpal7 ), palette[7].R, palette[7].G, palette[7].B);
-	Fl::get_color( RGBCOLOR( cfgpal8 ), palette[8].R, palette[8].G, palette[8].B);
+//	Fl::get_color( RGBCOLOR( cfgpal0 ), palette[0].R, palette[0].G, palette[0].B);
+//	Fl::get_color( RGBCOLOR( cfgpal1 ), palette[1].R, palette[1].G, palette[1].B);
+//	Fl::get_color( RGBCOLOR( cfgpal2 ), palette[2].R, palette[2].G, palette[2].B);
+//	Fl::get_color( RGBCOLOR( cfgpal3 ), palette[3].R, palette[3].G, palette[3].B);
+//	Fl::get_color( RGBCOLOR( cfgpal4 ), palette[4].R, palette[4].G, palette[4].B);
+//	Fl::get_color( RGBCOLOR( cfgpal5 ), palette[5].R, palette[5].G, palette[5].B);
+//	Fl::get_color( RGBCOLOR( cfgpal6 ), palette[6].R, palette[6].G, palette[6].B);
+//	Fl::get_color( RGBCOLOR( cfgpal7 ), palette[7].R, palette[7].G, palette[7].B);
+//	Fl::get_color( RGBCOLOR( cfgpal8 ), palette[8].R, palette[8].G, palette[8].B);
+
+	palette[0].R = ui_colors.cfgpal0.r; palette[0].G = ui_colors.cfgpal0.g; palette[0].B = ui_colors.cfgpal0.b;
+	palette[1].R = ui_colors.cfgpal1.r; palette[1].G = ui_colors.cfgpal1.g; palette[1].B = ui_colors.cfgpal1.b;
+	palette[2].R = ui_colors.cfgpal2.r; palette[2].G = ui_colors.cfgpal2.g; palette[2].B = ui_colors.cfgpal2.b;
+	palette[3].R = ui_colors.cfgpal3.r; palette[3].G = ui_colors.cfgpal3.g; palette[3].B = ui_colors.cfgpal3.b;
+	palette[4].R = ui_colors.cfgpal4.r; palette[4].G = ui_colors.cfgpal4.g; palette[4].B = ui_colors.cfgpal4.b;
+	palette[5].R = ui_colors.cfgpal5.r; palette[5].G = ui_colors.cfgpal5.g; palette[5].B = ui_colors.cfgpal5.b;
+	palette[6].R = ui_colors.cfgpal6.r; palette[6].G = ui_colors.cfgpal6.g; palette[6].B = ui_colors.cfgpal6.b;
+	palette[7].R = ui_colors.cfgpal7.r; palette[7].G = ui_colors.cfgpal7.g; palette[7].B = ui_colors.cfgpal7.b;
+	palette[8].R = ui_colors.cfgpal8.r; palette[8].G = ui_colors.cfgpal8.g; palette[8].B = ui_colors.cfgpal8.b;
 
 	wf->setcolors();
 	setColorButtons();

src/status/ui_colors.cxx
@@ -153,7 +153,6 @@ void UI_COLORS::load(std::string fname)
 			while (!ptr->var.empty()) {
 				if (ptr->var == chvar) {
 					ptr->r = triad.r; ptr->g = triad.g, ptr->b = triad.b;
-//			std::cout << "FOUND: " << chvar << ", " << (uint)triad.r << ", " << (uint)triad.g << ", " << (uint)triad.b << std::endl;
 					break;
 				}
 				ptr++;

src/waterfall/colorbox.cxx
@@ -69,6 +69,7 @@ void selectColor(int n)
 	
 	btnColor[n]->color( fl_rgb_color( palette[n].R, palette[n].G, palette[n].B ) );
 	btnColor[n]->redraw();
+
 	wf->setcolors();
 	WF_Palette->redraw();
 }
@@ -101,7 +102,8 @@ void loadPalette()
 			palette[i].G = g;
 			palette[i].B = b;
 		}
-   		fclose(clrfile);
+		fclose(clrfile);
+
 		wf->setcolors();
    		setColorButtons();
 		palfilename = p;

src/waterfall/waterfall.cxx
@@ -431,12 +431,13 @@ void WFdisp::makeScale() {
 
 void WFdisp::setcolors() {
 	double di;
-	int r, g, b;
+	int r, g, b, n;
 	for (int i = 0; i < 256; i++) {
 		di = sqrt((double)i / 256.0);
 		mag2RGBI[i].I = (uchar)(200*di);
 	}
-	for (int n = 0; n < 8; n++) {
+
+	for (n = 0; n < 8; n++) {
 		for (int i = 0; i < 32; i++) {
 			r = palette[n].R + (int)(1.0 * i * (palette[n+1].R - palette[n].R) / 32.0);
 			g = palette[n].G + (int)(1.0 * i * (palette[n+1].G - palette[n].G) / 32.0);
@@ -461,7 +462,8 @@ void WFdisp::initmaps() {
 	memset (mag2RGBI, 0, sizeof(mag2RGBI));
 	initMarkers();
 	makeScale();
-	setcolors();
+//std::cout << "WFdisp::initmaps()\n";
+//	setcolors();
 }
 
 int WFdisp::peakFreq(int f0, int delta)